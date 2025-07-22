100abccc4b7425840f33103e39f025f8ee4e36c7
Don Dave <davewabwile@gmail.com>
2025-07-22 11:24:58 +0300

    Create createDemoUser.js

---
+++ src/scripts/createDemoUser.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(async () => {
+    const email = 'admin@workguard360.com';
+    const password = 'demo123';
+    const user = await User.findOne({ email });
+    if (user) {
+      user.password = await bcrypt.hash(password, await bcrypt.genSalt(12));
+      await user.save();
+      console.log('Password updated for', email);
+    } else {
+      const newUser = await User.create({
+        name: 'Admin User',
+        email,
+        password: await bcrypt.hash(password, await bcrypt.genSalt(12)),
+        role: 'admin',
+        employeeId: 'EMP001',
+        department: 'Administration',
+      });
+      console.log('Demo user created:', newUser.email);
+    }
+    process.exit();
+  })
+  .catch(err => {
+    console.error('Error:', err);
+    process.exit(1);
+  });
