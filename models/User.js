63e0908e8018cf7dfa9c71970c0cd1fc40e19b3a
Don Dave <davewabwile@gmail.com>
2025-07-30 06:15:27 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -24,7 +24,3 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
