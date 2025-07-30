baf019e6780d0bf754d5d6e44af4529d9a9bbe24
Don Dave <davewabwile@gmail.com>
2025-07-30 04:43:55 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { loginUser } = require('../controllers/authController');
+const authController = require("../controllers/authController"); //
 
-router.post('/login', loginUser);
+router.post("/login", authController.login); // ✅ this must reference a defined function
 
 module.exports = router;
-
