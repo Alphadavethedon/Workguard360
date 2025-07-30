8d964da1282e8938ac575878767fa882c350b12d
Don Dave <davewabwile@gmail.com>
2025-07-30 06:42:46 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,26 +1,29 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     unique: true,
-    lowercase: true,
-    trim: true
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   role: {
     type: String,
-    enum: ["admin", "user"],
-    default: "user"
-  }
+    enum: ['admin', 'hr', 'security'],
+    default: 'security',
+  },
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-// Schema method for comparing password
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
+module.exports = mongoose.model('User', userSchema);
