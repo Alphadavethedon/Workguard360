26e6e022c9980c0b707ddbab2282d560417ac08d
Don Dave <davewabwile@gmail.com>
2025-08-01 02:37:57 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -10,5 +10,4 @@ app.use(cors({
 app.use(express.json());
 app.use('/api', authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app; // Export the app for Vercel
