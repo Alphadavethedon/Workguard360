522b8712aa1b14454cb17ae496ba7b2b209708e9
Don Dave <davewabwile@gmail.com>
2025-08-08 05:52:28 +0300

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,14 @@
+// backend/middlewares/errorHandler.js
+
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-  res.status(statusCode).json({
+  console.error(`[ERROR] ${err.message}`);
+  
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+
+  res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
   });
 };
 
