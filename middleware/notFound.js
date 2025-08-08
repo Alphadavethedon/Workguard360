f7ca40120f0165b5e796b958bb676a5a570dccce
Don Dave <davewabwile@gmail.com>
2025-08-08 06:09:16 +0300

    Update notFound.js

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,5 +1,8 @@
-const notFound = (req, res, next) => {
-  res.status(404).json({ message: `🔍 Not Found: ${req.originalUrl}` });
+const notFound = (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
 };
 
 module.exports = notFound;
