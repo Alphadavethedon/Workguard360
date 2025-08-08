b76ce578173e58debbe7e232a296f40423d83185
Don Dave <davewabwile@gmail.com>
2025-08-09 01:02:39 +0300

    Update logger.js

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,15 @@
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.colorize(),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console()
+  ]
+});
 
 module.exports = logger;
