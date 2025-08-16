a1e05c0fdc4570bc331e11be71844ca49ae5a4b7
Don Dave <davewabwile@gmail.com>
2025-08-16 03:51:55 +0300

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,6 @@
-const { createLogger, format, transports } = require('winston');
-
-const logger = createLogger({
-  level: 'info',
-  format: format.combine(
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    format.printf(info => `${info.timestamp} [${info.level}] ${info.message}`)
-  ),
-  transports: [new transports.Console()]
-});
-
-module.exports = logger;
+'use strict';
+module.exports = {
+  info: (...a) => console.log('[INFO]', ...a),
+  error: (...a) => console.error('[ERROR]', ...a),
+  warn: (...a) => console.warn('[WARN]', ...a),
+};
